308d86422ccd72f5782a2ae6f94d6ef6ca6b06dc
zhuzi <a1234@MacBook-Pro.local>
2023-11-20 14:05:12 +0800

    提交执行文件

--- pa6/Bounds3.hpp
+++ pa6/Bounds3.hpp
@@ -96,6 +96,9 @@ inline bool Bounds3::IntersectP(const Ray& ray, const Vector3f& invDir,
     // invDir: ray direction(x,y,z), invDir=(1.0/x,1.0/y,1.0/z), use this because Multiply is faster that Division
     // dirIsNeg: ray direction(x,y,z), dirIsNeg=[int(x>0),int(y>0),int(z>0)], use this to simplify your logic
     // TODO test if ray bound intersects
+
+
+    
     Vector3f intersectMin = (pMin - ray.origin) * invDir;
     Vector3f intersectMax = (pMax - ray.origin) * invDir;
     float iMin[3] = { intersectMin.x, intersectMin.y, intersectMin.z };
